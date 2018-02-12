3094212b237d803b9dd08282deb3b09986f431bb
jankimachhi <machhijanki282@gmail.com>
2018-02-12 15:34:48 +0530

    about updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,12 @@
 </head>
 <body>
 <h1>About Page</h1>
+<h1>About Page</h1>
+<h1>About Page</h1>
+<h1>About Page</h1>
+<h1>About Page</h1>
+<h1>About Page</h1>
+<h1>About Page</h1>
+
 </body>
 </html>
