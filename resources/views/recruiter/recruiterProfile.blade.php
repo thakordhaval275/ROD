4f3441652c6d1ea69712e5f1400782c36967074d
jankimachhi <machhijanki282@gmail.com>
2018-02-22 16:38:16 +0530

    update

--- resources/views/recruiter/recruiterProfile.blade.php
+++ resources/views/recruiter/recruiterProfile.blade.php
@@ -28,6 +28,39 @@
                             <p>An artist of considerable range, Chet Faker — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. </p>
                         </div>
                     </div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+                                <div class="col-md-6">
+                                    <div class="panel panel-danger">
+                                        <div class="panel-heading">Personal Details</div>
+                                        <div class="panel-body">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="col-md-4 col-sm-4 col-xs-4 col-lg-4">
+                                                        <h4>First Name</h4>
+                                                    </div><!--/col-4-->
+                                                    <div class="col-md-1 col-sm-1 col-xs-1 col-lg-1">
+                                                        <h4>:</h4>
+                                                    </div> <!--/col-1-->
+                                                    <div class="col-md-7 col-sm-7 col-xs-7 col-lg-7">
+                                                        <h5>Christian Louboutin</h5>
+                                                    </div><!--/col-7-->
+                                                </div><!--/col-12-->
+                                            </div><!--/row--><br>
+
+                                        </div><!--/panel-body-->
+                                    </div><!--/panel-->
+                                </div><!--/col-6-->
+                                <div class="col-md-6">
+                                    <div class="panel panel-danger">
+                                        <div class="panel-heading">Other Details</div>
+                                        <div class="panel-body">Panel Content</div>
+                                    </div>
+                                </div><!--/col-6-->
+                            </div><!--/col-12-->
+                        </div><!--/row-->
+                    </div><!--/container-->
                 </div>
             </div>
         </div>
