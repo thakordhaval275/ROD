e3972ecc9e01369727f172746f8e1ac3c65a06bb
thakor dhaval <thakor.dhaval275@gmail.com>
2018-02-22 11:11:45 +0530

    profile update

--- resources/views/recruiter/recruiterProfile.blade.php
+++ resources/views/recruiter/recruiterProfile.blade.php
@@ -5,26 +5,31 @@
 @endsection
 
 @section('contain')
-    <div class="container">
-        <br>
-        <div class="row">
-            <div class="panel panel-info">
-                <div class="panel-heading">Personal Details</div>
-                <div class="panel-body">
-                    <input type="text" name="fname" placeholder="name">
+    <div class="profile-page">
+        <div class="wrapper">
+            <div class="header header-filter" style="background-image: url('assets/img/employee/city.jpg');"></div>
+
+            <div class="main main-raised">
+                <div class="profile-content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="profile">
+                                <div class="avatar">
+                                    <img src="assets/img/employee/christian.jpg" alt="Circle Image" class="img-circle img-responsive img-raised">
+                                </div>
+                                <div class="name">
+                                    <h3 class="title">Christian Louboutin</h3>
+                                    <h6>Designer</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="description text-center">
+                            <p>An artist of considerable range, Chet Faker — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. </p>
+                        </div>
+                    </div>
                 </div>
-            </div><!--/panel-->
-        </div><!--/row-->
-
-        <div class="row">
-            <div class="panel panel-danger">
-                <div class="panel-heading">Personal Details</div>
-                <div class="panel-body">
-                    <input type="text"  class="text-center" name="fname" placeholder="name">
-                </div>
-            </div><!--/panel-->
-        </div><!--/row-->
-
-    </div><!--/container-->
-
+            </div>
+        </div>
+    </div>
+    <br><br>
 @endsection
