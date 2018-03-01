bf150ae047174ed2f52079fa557fe04549016fcb
thakor dhaval <thakor.dhaval275@gmail.com>
2018-03-01 15:03:09 +0530

    update company profile

--- resources/views/company/companyProfile.blade.php
+++ resources/views/company/companyProfile.blade.php
@@ -32,6 +32,7 @@
                         <div class="row">
                             <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
                                 <div class="col-md-2">
+                                   
                                 </div>
                                 <div class="col-md-8">
                                     <div class="panel panel-danger">
