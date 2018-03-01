c5015e782f69be184847da1988cf82d1fc4998ba
thakor dhaval <thakor.dhaval275@gmail.com>
2018-03-01 10:14:22 +0530

    update changes

--- resources/views/company/companyProfile.blade.php
+++ resources/views/company/companyProfile.blade.php
@@ -15,7 +15,7 @@
                         <div class="row">
                             <div class="profile">
                                 <div class="avatar">
-                                    <img src="assets/img/employee/christian.jpg" alt="Circle Image" class="img-circle img-responsive img-raised"><br><a href="{{route('companyEdit')}}"><i class="ti-pencil"> Edit</i></a>
+                                    <img src="assets/img/company/Mtaj.png" alt="Circle Image" class="img-circle img-responsive img-raised"><br><a href="{{route('companyEdit')}}"><i class="ti-pencil"> Edit</i></a>
                                 </div>
                                 <div class="name">
                                     <h3 class="title">MTAJ Solutions PVT LTD</h3>
@@ -31,14 +31,31 @@
                         <br><br>
                         <div class="row">
                             <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
-                                <div class="col-md-12">
+                                <div class="col-md-2">
+                                </div>
+                                <div class="col-md-8">
                                     <div class="panel panel-danger">
-                                        <div class="panel-heading">Personal Details</div>
+                                        <div class="panel-heading">Company Details<a href="{{route('companyEdit')}}" style="float: right;color: white;"><i class="ti-pencil"></i></a></div>
                                         <div class="panel-body">
-
+                                            <br>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="col-md-4 col-sm-4 col-xs-4 col-lg-4">
+                                                        <h4>Company Name</h4>
+                                                    </div>
+                                                    <div class="col-md-1 col-sm-1 col-xs-1 col-lg-1">
+                                                        <h4>:</h4>
+                                                    </div>
+                                                    <div class="col-md-7 col-sm-7 col-xs-7 col-lg-7">
+                                                        <h5>Louboutin</h5>
+                                                    </div>
+                                                </div>
+                                            </div><br>
                                         </div>
                                     </div>
                                  </div>
+                                <div class="col-md-2">
+                                </div>
                             </div>
                         </div>
                     </div>
