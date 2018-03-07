5fed742dacfdf913e816060106daf0f557e4d678
thakor dhaval <thakor.dhaval275@gmail.com>
2018-03-07 10:32:11 +0530

    update

--- resources/views/company/viewPostJobs.blade.php
+++ resources/views/company/viewPostJobs.blade.php
@@ -11,7 +11,7 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="row">
-						<div class="col-md-12 box text-right">
+						<div class="col-md-12 text-right">
 							<a href="{{route('jobpost')}}"><div class="btn btn-common btn-rm">Add New Job</div></a>
 						</div>
 					</div>
