21119022d61ab9ead34814c5989a338844b01f91
thakor dhaval <thakor.dhaval275@gmail.com>
2018-03-08 17:04:41 +0530

    update route

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,17 @@ Route::get('/', function () {
 
 Route::get('/login','RodController@login')->name('login'); //Login
 
+//=================================================================================
 //Home Pages
 Route::get('/index','RodController@index')->name('index'); //index
 Route::get('/about','RodController@about')->name('about'); //about
 Route::get('/contact','RodController@contact')->name('contactus'); //contect
 Route::get('/signup','RodController@signup')->name('signup'); //signup
 
-
 //store Data
 Route::post('contactstore','RodController@contactstore')->name('contactstore');
 
-
-
+//=================================================================================
 //Company Pages
 Route::get('/companyjobpost','companyController@jobpost')->name('jobpost'); //jobpost
 Route::get('/companyprofile','companyController@companyprofile')->name('companyProfile');//company Profile
@@ -40,7 +39,7 @@ Route::get('/jobdetail','companyController@jobdetail')->name('jobDetail');//Job 
 //sotre Data
 Route::post('jobpoststore','companyController@jobpoststore')->name('jobpoststore');
 
-
+//=================================================================================
 //Recruiter Pages
 Route::get('/recruiterprofile','recruiterController@recruiterprofile')->name('recruiterProfile');//recruiter Profile
 Route::get('/recruiterEdit','recruiterController@recruiteredit')->name('recruiterEdit');//recruiter Profile
@@ -48,12 +47,15 @@ Route::get('/addmyemployee','recruiterController@addmyemployee')->name('addmyEmp
 Route::get('/myemployee','recruiterController@myemployee')->name('myEmployee');//recruiter's employee
 Route::get('/proposal','recruiterController@proposal')->name('proposal');//send a Proposal
 
+
+//=================================================================================
 //Employee Pages
 Route::get('/employeeprofile','employeeController@employeeprofile')->name('employeeProfile');//Employee Profile
 Route::get('/employeeedit','employeeController@employeeedit')->name('employeeEdit');//Edit Profile
 
+//=================================================================================
 //Admin Pages
-Route::get('/admin','adminController@index')->name('index');//Admin Index
+Route::get('/admin','adminController@index')->name('admin');//Admin Index
 Route::get('/admin/companyslist','adminController@companylist')->name('companyList');//company List
 Route::get('/admin/recruiterlist','adminController@recruiterlist')->name('recruiterList');//Recruiter List
 Route::get('/admin/employeelist','adminController@employeelist')->name('employeeList');//Employee List
