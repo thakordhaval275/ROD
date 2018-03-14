c6e4cb9e1692abce555a82a469077a409d2f18c7
thakor dhaval <thakor.dhaval275@gmail.com>
2018-03-14 13:01:55 +0530

    route change

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::get('/admin','adminController@index')->name('admin');//Admin Index
 
 //Company Pages
 Route::get('/admin/company/addcompany','adminController@addcompany')->name('addCompany');//Admin Add New Company
-Route::get('/admin/company/viewcompany/{id}','adminController@viewcompany')->name('viewCompany');//Admin company Profile
+    Route::get('/admin/company/viewcompany/{id}','adminController@viewcompany')->name('viewCompany');//Admin company Profile
 
 Route::get('/admin/company/companyslist','adminController@companylist')->name('companyList');//Admin company List
 Route::get('/admin/company/jobpostlist','adminController@jobpostlist')->name('postJobList');//Admin Job Post List
