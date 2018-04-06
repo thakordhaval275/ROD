aa8b7c4ac8efcedc29de54d0a8d4b70f108c8f99
jankimachhi <machhijanki282@gmail.com>
2018-04-06 14:46:41 +0530

    change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -196,39 +196,41 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="row">
-                    @foreach($company as $company)
-                        @if(count($company)<=3)
-                            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 blog-item">
-                                <!-- Blog Item Starts -->
-                                <div class="blog-item-wrapper">
-                                    <div class="blog-item-img">
-                                        <a href="single-post.html">
-                                            <img src="{{ asset('assets/img/company').'/'.$company->logo}}" width="70%" height="70%">
-                                        </a>
-                                    </div>
-                                    <div class="blog-item-text">
-                                        <div class="meta-tags">
-                                            <span class="date"><i class="ti-calendar"></i> 22-1-18</span>
-                                            <span class="comments"><a href="#"><i class="ti-comment-alt"></i> 5 Comments</a></span>
-                                        </div>
-                                        <a href="#">
-                                            <h3>
-                                                {{$company->companyname}}
-                                            </h3>
-                                        </a>
-                                        <h6>Location:
-                                            {{$company->address}},&nbsp;{{$company->location}}
-                                        </h6>
-                                        <h6>
-                                            Website:{{$company->website}}
-                                        </h6>
-                                        <a href="#" class="btn btn-common btn-rm">Read More</a>
-                                    </div>
-                                </div>
-                            @endif
-                    </div>
+                    @if(!Auth::user())
+                    @endif
+                    {{--@foreach($company as $company)--}}
+                        {{--@if(count($company)<=3)--}}
+                            {{--<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 blog-item">--}}
+                                {{--<!-- Blog Item Starts -->--}}
+                                {{--<div class="blog-item-wrapper">--}}
+                                    {{--<div class="blog-item-img">--}}
+                                        {{--<a href="single-post.html">--}}
+                                            {{--<img src="{{ asset('assets/img/company').'/'.$company->logo}}" width="70%" height="70%">--}}
+                                        {{--</a>--}}
+                                    {{--</div>--}}
+                                    {{--<div class="blog-item-text">--}}
+                                        {{--<div class="meta-tags">--}}
+                                            {{--<span class="date"><i class="ti-calendar"></i> 22-1-18</span>--}}
+                                            {{--<span class="comments"><a href="#"><i class="ti-comment-alt"></i> 5 Comments</a></span>--}}
+                                        {{--</div>--}}
+                                        {{--<a href="#">--}}
+                                            {{--<h3>--}}
+                                                {{--{{$company->companyname}}--}}
+                                            {{--</h3>--}}
+                                        {{--</a>--}}
+                                        {{--<h6>Location:--}}
+                                            {{--{{$company->address}},&nbsp;{{$company->location}}--}}
+                                        {{--</h6>--}}
+                                        {{--<h6>--}}
+                                            {{--Website:{{$company->website}}--}}
+                                        {{--</h6>--}}
+                                        {{--<a href="#" class="btn btn-common btn-rm">Read More</a>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--@endif--}}
+                    {{--</div>--}}
 
-                        @endforeach
+                        {{--@endforeach--}}
                 </div>
             </div>
         </div>
